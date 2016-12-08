e94dca940eed013576a0ac9ad017c3019df5ef67
Kevin Cook <kevinac4@gmail.com>
2016-12-07 16:03:32 -0800

    adding siteurl

--- app/js/constants.js
+++ app/js/constants.js
@@ -7,6 +7,7 @@ if(window.location.port !== ''){
 	  backendUrl: 'http://api.respondreact.com:8100',
 	  mediaUrl: 'http://media.respondreact.com:3100',
 	  googleApiKey: 'AIzaSyAztcWr5QxKQB2tBPwTAbqgIggtUHu4D1M',
+	  siteUrl: 'http://respondreact.com:3000',
 	};
 } else {
 	CONFIG = {
@@ -15,6 +16,7 @@ if(window.location.port !== ''){
 	  backendUrl: 'http://api.respondreact.com',
 	  mediaUrl: 'http://media.respondreact.com',
 	  googleApiKey: 'AIzaSyAztcWr5QxKQB2tBPwTAbqgIggtUHu4D1M',
+	  siteUrl: 'http://respondreact.com',
 	};
 }
 
