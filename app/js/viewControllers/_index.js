a9e2d6c86b639828ae554cf81572c14ffe213b9b
Kevin Cook <kevinac4@gmail.com>
2017-01-06 00:35:20 -0800

    footer controller

--- app/js/viewControllers/_index.js
+++ app/js/viewControllers/_index.js
@@ -4,5 +4,3 @@ const angular = require('angular');
 
 exports = angular.module('viewControllers', [])
             .controller('FooterCtrl', require('./footer-controller'));
-            // .controller('NavBarCtrl', require('./navbar-controller')),
-            // .controller('ModalCtrl', require('./modal-controller'));
